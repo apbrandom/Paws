2ce32d24b96620c07d48abc7efe39da73ce06485
apbrandom <apbrandom@gmail.com>
2025-05-05 21:21:25 +0200

    Initial Commit

---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by Vadim Vinogradov on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
